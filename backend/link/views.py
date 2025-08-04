64ef1c6e8a49f95042e5f20c19f7b30137e95430
med-macbook <med.rk000@gmail.com>
2025-08-04 22:56:15 +0900

    link集の完全完成

--- backend/link/views.py
+++ backend/link/views.py
@@ -4,4 +4,5 @@ from .models import Link,Category
 def link(request):
     links=Link.objects.all()
     categories=Category.objects.all()
+    links=links.filter(public=True)
     return render(request,'link/link_base.html',{'links':links,'categories':categories})
